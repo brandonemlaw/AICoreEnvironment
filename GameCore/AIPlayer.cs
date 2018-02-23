0e367a25a7510fd88c0deba7edac4f212dd1796d
Brandon Emlaw <bemlaw@HARDING.EDU>
2018-02-22 21:53:34 -0600

    Revert "AIPlayer Longs"
    This reverts commit c1ca2a91dd4d28d497ec117e05409e0fdf4059fe.

--- GameCore/AIPlayer.cs
+++ GameCore/AIPlayer.cs
@@ -21,7 +21,7 @@ namespace GameCore
 
         //Imports C++ AI DLL
         [DllImport(@"AICoreD.dll")]
-        private static extern AIMove AIGetMove(int blackCount, int whiteCount, ulong black, ulong white, bool isWhitesTurn, uint mode);
+        private static extern AIMove AIGetMove(int blackCount, int whiteCount, uint[] blackRows, uint[] whiteRows, bool isWhitesTurn, uint mode);
 
 
         public static  Move getAMove(bool isWhitesTurn)
@@ -34,39 +34,36 @@ namespace GameCore
             Board board = Program.getBoard();
 
             //Convert to AISpace
-            ulong[,] AISQUARES = { { 1, 2, 4, 8, 16, 32, 64, 128 },
-                { 256, 512, 1024, 2048, 4096, 8192, 16384, 32768 },
-                { 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608 },
-                {  16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648 },
-                { 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888 },
-                { 1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664, 140737488355328 },
-                { 281474976710656, 562949953421312, 1125899906842624, 2251799813685248, 4503599627370496, 9007199254740992, 18014398509481984, 36028797018963968 },
-                { 72057594037927936, 144115188075855872, 288230376151711744, 576460752303423488,
-                    1152921504606846976, 2305843009213693952, 4611686018427387904, 9223372036854775808}
-            };
+            uint[] AICOLUMNS = new uint[] { 1, 2, 4, 8, 16, 32, 64, 128 }; //A-H
+
+            uint[] blackRows = new uint[8];
+            uint[] whiteRows = new uint[8];
 
 
-            ulong black = 0;
-            ulong white = 0;
-
-            for (uint i = 0; i < 8; i++)
+            for (int i = 0; i < 8; i++)
             {
-                for (uint j = 0; j < 8; j++)
+                blackRows[i] = 0;
+                whiteRows[i] = 0;
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
                 {
                     if (board.blackRows[i] % board.COLUMNS[j] == 0)
                     {
-                        black += AISQUARES[i, j];
+                        blackRows[i] += AICOLUMNS[j];
                     }
                     if (board.whiteRows[i] % board.COLUMNS[j] == 0)
                     {
-                        white += AISQUARES[i, j];
+                        whiteRows[i] += AICOLUMNS[j];
                     }
                 }
             }
 
 
             //Get the move from the AI DLL
-            AIMove nextMove = AIGetMove(board.blackCount, board.whiteCount, black, white, isWhitesTurn, 0);
+            AIMove nextMove = AIGetMove(board.blackCount, board.whiteCount, blackRows, whiteRows, isWhitesTurn, 0);
 
             //Convert the AIMove to a Move class
             result.Begin.X = checked((int)nextMove.row);
